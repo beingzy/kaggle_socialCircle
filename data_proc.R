155a4d862d5c9396aecf33b5b05c453bcbf33753
Yi Zhang <beingzy@gmail.com>
2014-07-05 11:36:28 -0400

    Add data processing functionnality: convert egonents, circles, feature into JSON-ready text

--- data_proc.R
+++ data_proc.R
@@ -19,12 +19,12 @@ library(stringr)
 # SETUP ENVIRONMENT           #
 # ########################### #
 repo        <- list()
-repo$data   <- c("C:/A-PlayGround/socialCircle/data/")
-repo$output <- c("C:/A-PlayGround/socialCircle/output/")
+repo$data   <- c("/Users/beingzy/Documents/kaggle/socialNetwork/data/")
+repo$output <- c("/Users/beingzy/Documents/kaggle/socialNetwork/output/")
  
 filesnm               <- list()
-filesnm$egnonets      <- list.files(path="C:/A-PlayGround/socialCircle/data/egonets",  full.names = T)
-filesnm$circles_train <- list.files(path="C:/A-PlayGround/socialCircle/data/training", full.names = T)
+filesnm$egnonets      <- list.files(path=paste(repo$data, "/egonets/", sep=""),  full.names = T)
+filesnm$circles_train <- list.files(path=paste(repo$data, "/Training/", sep=""), full.names = T)
  
 # ###################### #
 # FUNCTION DEFINIITION   #
@@ -56,7 +56,7 @@ for(temp.i in 1:length(filesnm$egnonets)){
     meta_info     <- cbind(meta_info , c(temp_loop$user_id, length(temp_loop$item_user_id)))
   }
 }
-rm(temp_loop)
+rm(list = c("temp.i", "temp.j", "temp_loop"))
 ## Format the meta data information (user_id, total of friends)
 meta_info             <- t(meta_info)
 meta_info             <- data.frame(meta_info)
@@ -71,9 +71,33 @@ write.table(x=meta_info,     file=getDataPath("user_relations_meta_info.csv", di
 # Convert circle data into a list of JSON #
 # objects                                 #
 # ####################################### #
+temp_loop  <- list()
+circles    <- c()
+meta_info  <- c()
 
-
-
+for(temp.i in 1:length(filesnm$circles_train)) {
+  temp_loop$f <- readLines(con=filesnm$circles_train[temp.i])
+  for(temp.j in 1:length(temp_loop$f)){
+    temp_loop$f_text              <- strsplit(x=temp_loop$f[temp.j], split=":")[[1]]
+    temp_loop$circle_id           <- temp_loop$f_text[1]
+    temp_loop$circled_user_id_str <- gsub(pattern=" ", replacement=",", str_trim( f_text[2] ))
+    temp_loop$circled_user_id_vec <- strsplit(str_trim(temp_loop$f_text[2]), split=" ")[[1]]
+    temp_loop$circle_json         <- paste("{circle_id:", temp_loop$circle_id, ",",
+                                            "user_id:[", temp_loop$circled_user_id_str, "]}", sep = "")
+    circles   <- c(circles, temp_loop$circle_json)
+    meta_info <- cbind(meta_info , c(temp_loop$circle_id, length(temp_loop$circled_user_id_vec)))
+  }
+}
+rm(list = c("temp.i", "temp.j", "temp_loop"))
+## Format the meta data information (circle_id, number of circled users)
+meta_info                 <- t(meta_info)
+meta_info                 <- data.frame(meta_info)
+colnames(meta_info)       <- c("circle_id", "circled_user_id")
+meta_info$circle_id       <- as.character(meta_info$circle_id)
+meta_info$circled_user_id <- as.numeric(meta_info$circled_user_id)
+## Export the formated information
+write.table(x=circles,   file=getDataPath("socialCircles_json.txt", dir=repo$output),      sep="\n", row.names=F, col.names=F)
+write.table(x=meta_info, file=getDataPath("socialCircles_meta_info.csv", dir=repo$output), sep="\n", row.names=F, col.names=T)
 
 
 
